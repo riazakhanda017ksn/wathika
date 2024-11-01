5003e2822f064d5e78b1c18e739078aae3609411
riazakhanda017ksn <riazakhanda@gmail.com>
2024-11-02 01:19:09 +0600

    commit

---
+++ assets/js/login.js
@@ -0,0 +1,6 @@
+function toggleForm() {
+    const signInForm = document.getElementById('signInForm');
+    const signUpForm = document.getElementById('signUpForm');
+    signInForm.classList.toggle('hidden');
+    signUpForm.classList.toggle('hidden');
+  }
